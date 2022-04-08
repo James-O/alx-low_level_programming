bcd7b660a73363acbf54b25e0702b972a26b19f1
James Ogbonna <ogbonnasunday42@gmail.com>
2022-04-08 14:45:50 -0700

    complex

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,11 +17,11 @@ while (c < 10)
 {
 if (d != c && d < c)
 {
-putchar('0' + d); 
-putchar('0' + c);                          
+putchar('0' + d);
+putchar('0' + c);
 if (c + d != 17)
-{                                 
-putchar(',');                                 
+{
+putchar(',');
 putchar(' ');
 }
 }
