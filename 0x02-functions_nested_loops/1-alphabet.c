b0b086e79d8a9a89afeee240f11e67baaf1c1e65
James Ogbonna <ogbonnasunday42@gmail.com>
2022-04-12 10:11:21 -0700

    lower alphabet fxn

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - check the code
+*
+*Return: Always 0.
+*/
+void print_alphabet(void)
+{
+char c = 'a';
+
+while (c <= 'z')
+{
+c++;
+_putchar(c);
+_putchar('\n');
+}
+}
