52b1fc753df0af4a8ebcbbd0e283ccf9dbc59da9
James Ogbonna <ogbonnasunday42@gmail.com>
2022-04-13 06:59:12 -0700

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test function for positive_or_negetive
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	positive_or_negetive(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+First debugging readme file
