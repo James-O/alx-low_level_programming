09fae48fc0ccb9c62dd4f2eb15bb1867c6a3f150
James Ogbonna <ogbonnasunday42@gmail.com>
2022-05-26 18:58:12 -0700

    func that frees a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Funtions that free a list head = NULL
+ *
+ * @head: pointer
+ *
+ *Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (!head)
+		return;
+	while (*head)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+	*head = NULL;
+}
